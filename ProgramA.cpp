2b7e7464410c8ffd162c9d9b237ad6d10561b333
tmatth11 <tmmatthews11@gmail.com>
2024-10-03 21:12:35 -0500

    Created opening comment for ProgramA

---
+++ ProgramA.cpp
@@ -0,0 +1,43 @@
+/*
+    Programming Language and IDE:
+    The programming language used is C++, and the IDE is Visual Studio Code.
+
+    Compiling, Building, and Executing:
+    Click the arrow next to the play button in the top right corner of the screen and select "Run Code" to compile and run the program.
+
+    Programmer(s):
+    Tony Matthews
+
+    Date of Program Creation:
+    10/03/2024
+
+    Course Name:
+    CMP SCI 4500-001
+
+    Program Explanation:
+    This program reads filenames from the current directory that match the pattern of "XLog.csv" and validates their format as time log files. It then outputs the results to the screen and an external file. Validity checks include ensuring the file is not empty, has correctly formatted entries, and doesn't contain invalid time entries.
+
+    Central Data Structure(s):
+
+
+    External Files Used:
+    XLog.csv
+
+    Outside Resources:
+
+    Source:
+    Information:
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+
+    // Pauses the program in exe file to see the output
+    system("pause");
+    return 0;
+}
