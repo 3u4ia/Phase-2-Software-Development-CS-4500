537090815a0cf1fc3c617028b5503edb8393a9ac
tmatth11 <141206635+tmatth11@users.noreply.github.com>
2024-10-20 21:54:04 -0500

    Fixed opening comment format for Program B

--- ProgramB.cpp
+++ ProgramB.cpp
@@ -1,17 +1,13 @@
-/*
-    Programming Language and IDE:
-    The programming language used is C++, and the IDE is Visual Studio Code.
+/*  Programming Language and IDE: The programming language used is C++, and the IDE is Visual Studio Code.
 
-    Compiling, Building, and Executing:
-    Click the arrow next to the play button in the top right corner of the screen and select "Run Code" to compile and run the program.
+    Compiling, Building, and Executing: Click the arrow next to the play button in the top right corner of the screen and select "Run Code" to compile and run the program.
 
     Programmer(s):
     Mary Lorenz
     Elroi Belete
     Tony Matthews
 
-    Date of Program Creation:
-    10/04/2024
+    Date of Program Creation: 10/04/2024
 
     Program Explanation:
     This program looks for a specific user's time log file based on their first and last name.
@@ -19,15 +15,13 @@
     The program then checks the validity of the log file and detects any errors in the log entries.
     If no errors are found, the program creates a report based on how many minutes are spent on each activity.
 
-    Course:
-    CMP SCI 4500-001
+    Course: CMP SCI 4500-001
 
     Central Data Structure(s):
     - activityDescriptions: A map that stores activity codes and their corresponding descriptions.
     - activityMinutes: A map that stores activity codes and the total minutes spent on each activity.
 
-    External Files Used:
-    LastnameFirstnameLog.csv
+    External Files Used: LastnameFirstnameLog.csv
 
     Outside Resources:
     ProgramA.cpp, TeamXLogEntryLater.Y
